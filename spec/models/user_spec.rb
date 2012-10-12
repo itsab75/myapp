96ab64c11400bed788efc68dd85ea7f44a4902bf
itsab75 <itsab75@gmail.com>
2012-10-12 14:33:34 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,9 @@ describe User do
                      password: "foobar", password_confirmation: "foobar")
   end
 
+
+
+
   subject { @user }
 
   it { should respond_to(:name) }
@@ -25,8 +28,17 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
   
   it { should be_valid }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
