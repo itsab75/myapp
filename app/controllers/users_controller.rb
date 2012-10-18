b24e067473a3cb10d280546d266b6be4eae41195
itsab75 <itsab75@gmail.com>
2012-10-18 10:44:35 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -40,31 +41,31 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+#    def signed_in_user
+#      unless signed_in?
+#        store_location
+#        redirect_to signin_url, notice: "Please sign in."
+#      end
+#    end
 
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
 
-  def admin_user
+    def admin_user
       redirect_to(root_path) unless current_user.admin?
-  end
+    end
     
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
-  end
   
-  
-
 end
---
+++ spec/requests/authentication_pages_spec copy.rb
@@ -0,0 +1,145 @@
+# require 'spec_helper'
+
+# describe "AuthenticationPages" do
+#  describe "GET /authentication_pages" do
+#    it "works! (now write some real specs)" do
+#      Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get authentication_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+# end
+
+require 'spec_helper'
+
+describe "Authentication" do  #1
+
+  subject { page }
+
+  describe "signin page" do 
+    before { visit signin_path }
+
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end # signin page
+  
+  describe "signin" do #2
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end # with invalid informtion
+
+    describe "with valid information" do #3 
+      let(:user) { FactoryGirl.create(:user) }
+        before { sign_in user }
+
+        before do
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_link('Users',    href: users_path) }
+        it { should have_link('Profile', href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
+        it { should have_link('Sign out', href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
+    end #with valid information
+    
+    describe "after saving the user" do
+#        it { should have_link('Sign out') }
+    end
+
+    describe "followed by signout" do
+#        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+    end
+      
+  end # signin
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end # in the Users controller
+    end # for non-signed in users
+
+
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end # after signing in
+      
+      end # when attempting to visit a protected page
+
+#      describe "as non-admin user" do
+#        let(:user) { FactoryGirl.create(:user) }
+#        let(:non_admin) { FactoryGirl.create(:user) }
+#        before { sign_in non_admin }
+
+#        describe "submitting a DELETE request to the Users#destroy action" do
+#          before { delete user_path(user) }
+#          specify { response.should redirect_to(root_path) }        
+#        end
+#      end # as non-admin user
+
+    
+      describe "as wrong user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+        before { sign_in user }
+
+        describe "visiting Users#edit page" do
+          before { visit edit_user_path(wrong_user) }
+          it { should_not have_selector('title', text: full_title('Edit user')) }
+        end
+
+        describe "submitting a PUT request to the Users#update action" do
+          before { put user_path(wrong_user) }
+          specify { response.should redirect_to(root_path) }
+        end
+      end # as wrong user
+    end # for non signed-in users   
+
+  end #Authorization
+end  #Authentication
